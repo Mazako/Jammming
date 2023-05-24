bd1e1ac13fff81068798cacf3b25e90b4004eaf8
mazak <michal.maziarz12@gmail.com>
2023-05-24 13:11:50 +0200

    Alpha

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Logo from './logo/Logo'
+import './App.css'
+function App() {
+  return (
+      <Logo />
+  )
+}
+
+export default App;
---
+++ src/logo/Logo.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './Logo.css'
+
+function Logo() {
+    return (
+        <div id='logo-div'>
+            <h1 className='logo'>Dawoj <span>plej</span>liste</h1>
+        </div>
+    )
+}
+
+export default Logo
